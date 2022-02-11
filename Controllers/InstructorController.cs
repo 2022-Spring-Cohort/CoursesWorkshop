a88b21ae29035bb8417fc6c4a04ded9949e4a2c5
gavhensley <gavinrhensley@gmail.com>
2022-02-11 11:52:34 -0500

    added instructor index view

---
+++ Controllers/InstructorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoursesWorkshop.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private ApplicationContext _context;
+        public InstructorController()
+        {
+            _context = new ApplicationContext();
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Instructors.ToList());
+        }
+    }
+}
